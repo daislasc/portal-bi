4a421f44a31b2f162ab01f0a48ea05a99874e901
daislas <daislas@deacero.com>
2025-09-12 09:49:22 -0600

    añadir inventarios al inventory summary

--- st.py
+++ st.py
@@ -41,18 +41,6 @@ dashboards = {
         },
         # 🔹 Estructura nueva: cada dashboard tiene Plantilla + Documentación
         "documentacion": {
-            "Producción": {
-                "plantilla": {
-                    "url": "",
-                    "desc": "Personaliza tu propio reporte de Producción"
-                },
-                "documentación": {
-                    "url": "https://deacero.atlassian.net/wiki/external/NjU0NGE5MWQyZjY0NDcxOTk1Nzk2MGM3YzA2YTk5NmQ",
-                    "desc": "Documentación para Autoservicio de Reporte de Producción"
-                },
-                "color": "#18515F"
-            },
-
             "Embarques y Facturas": {
                 "plantilla": {
                     "url": "https://lookerstudio.google.com/u/0/reporting/96bd7f9a-ad9c-4b02-a866-28dab9b0816c/page/p_ee36dyg2vd/preview",
